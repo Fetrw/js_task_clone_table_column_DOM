88c9eafe5de7fa3aa4677fa48b8782c854391b0d
MelnykVlad <21vladmel15@gmail.com>
2024-06-28 13:36:12 +0300

    make build

---
+++ index.6c802bc7.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("tbody tr"),t=document.querySelector("thead tr"),r=document.querySelector("tfoot tr"),o=function(e){var t=e.querySelectorAll("td, th"),r=t[1].cloneNode(!0),o=t.length-1;e.insertBefore(r,t[o])};e.forEach(function(e){return o(e)}),o(t),o(r);
+//# sourceMappingURL=index.6c802bc7.js.map
